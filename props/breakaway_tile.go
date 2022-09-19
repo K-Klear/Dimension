dcfa7887605f1a167e2dc46d9d98cd2151856d62
Klear <kingklear@gmail.com>
2022-09-20 01:42:22 +0200

    Added tile-busting effect when the first cube is summoned and finalised design of the first level.
    I think I'll skip level transitions for now. Though maybe I could try and inside-out cube...

---
+++ props/breakaway_tile.go
@@ -0,0 +1,35 @@
+components {
+  id: "breakaway_tile"
+  component: "/props/breakaway_tile.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/props/props.atlas\"\n"
+  "default_animation: \"breakaway_tile\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

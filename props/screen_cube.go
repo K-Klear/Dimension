35c037be9f05d6a1a6dd0467ccd60b97babeba7f
Klear <kingklear@gmail.com>
2022-09-21 02:32:39 +0200

    Added sounds. Polished the intro some more. Game now renders correctly after resizing window.

--- props/screen_cube.go
+++ props/screen_cube.go
@@ -2,7 +2,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/props/props.atlas\"\n"
-  "default_animation: \"screen_cube_blank\"\n"
+  "default_animation: \"noise_large\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -10,6 +10,26 @@ embedded_components {
     x: 0.0
     y: 0.0
     z: 0.5
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "light"
+  type: "sprite"
+  data: "tile_set: \"/light/light.atlas\"\n"
+  "default_animation: \"screen_large\"\n"
+  "material: \"/render/light.material\"\n"
+  "blend_mode: BLEND_MODE_ADD\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.6
   }
   rotation {
     x: 0.0
